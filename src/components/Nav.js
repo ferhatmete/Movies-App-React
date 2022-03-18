495308c0e264c512913d0a9c8a2552f12c231424
Ferhat METE <ferhatmete9224@gmail.com>
2022-03-18 19:42:56 +0300

    Create Navbar Nav.js

---
+++ src/components/Nav.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "../Styles/Nav.css";
+import { Link } from "react-router-dom";
+
+function Nav() {
+  return (
+    <nav>
+      <div className="nav__logo">
+        <Link to="/">WCIV</Link>
+      </div>
+      <div className="nav__links">
+        <Link to="/">Home</Link>
+        <Link to="trending">Trending</Link>
+        <Link to="top-rated">Top Rated</Link>
+        <Link to="action-movies">Action Movies</Link>
+        <Link to="comedy-movies">Comedy Movies</Link>
+        <Link to="horror-movies">Horror Movies</Link>
+        <Link to="romance-movies">Romance Movies</Link>
+        <Link to="documentaries">Documentaries</Link>
+      </div>
+    </nav>
+  );
+}
+
+export default Nav;
