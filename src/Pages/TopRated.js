bc39fb662bcd992e9236e4ad16107392906f4bf6
Ferhat METE <ferhatmete9224@gmail.com>
2022-03-18 19:50:08 +0300

    Setup Top Rated page

---
+++ src/Pages/TopRated.js
@@ -0,0 +1,9 @@
+import React from "react";
+// Components
+import Pages from "../components/Pages";
+
+function TopRated({ title, fetchUrl }) {
+  return <Pages title={title} fetchUrl={fetchUrl} />;
+}
+
+export default TopRated;
